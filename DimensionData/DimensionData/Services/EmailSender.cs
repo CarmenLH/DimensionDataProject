c5f86e9f5176915e93a259ac5dc0e8b6047f8068
CarmenLH <61046574+CarmenLH@users.noreply.github.com>
2020-11-20 20:27:09 +0200

    SMTP email service instead of SendGrid

--- DimensionData/DimensionData/Services/EmailSender.cs
+++ DimensionData/DimensionData/Services/EmailSender.cs
@@ -1,43 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
+using DimensionData.Models;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
-using SendGrid;
-using SendGrid.Helpers.Mail;
+using MimeKit;
+//using SendGrid;
+//using SendGrid.Helpers.Mail;
 
 namespace DimensionData.Services
 {
     public class EmailSender : IEmailSender
     {
-        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
+
+        private readonly EmailSettings _emailSettings;
+
+        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor, IOptions<EmailSettings> emailSettings)
         {
             Options = optionsAccessor.Value;
+            _emailSettings = emailSettings.Value;
         }
 
         public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
 
         public Task SendEmailAsync(string email, string subject, string message)
         {
-            return Execute(Options.SendGridKey, subject, message, email);
-        }
 
-        public Task Execute(string apiKey, string subject, string message, string email)
-        {
-            var client = new SendGridClient("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");
-            var msg = new SendGridMessage()
+            var client = new SmtpClient(_emailSettings.MailServer, _emailSettings.MailPort)
             {
-                From = new EmailAddress("mycvwebsiteemail@gmail.com", Options.SendGridUser),
-                Subject = subject,
-                PlainTextContent = message,
-                HtmlContent = message
+                Port = _emailSettings.MailPort,
+                UseDefaultCredentials = false,
+                EnableSsl = true,
+                Credentials = new NetworkCredential(_emailSettings.Sender, _emailSettings.Password)
             };
-            msg.AddTo(new EmailAddress(email));
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(_emailSettings.Sender),
+                Subject = subject,
+                Body = message,
+                IsBodyHtml = true
+            };
 
-            msg.SetClickTracking(false, false);
+            mailMessage.To.Add(email);
 
-            return client.SendEmailAsync(msg);
+            return client.SendMailAsync(mailMessage);
         }
     }
 }
